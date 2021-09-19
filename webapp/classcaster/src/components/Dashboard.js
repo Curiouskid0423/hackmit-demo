cc157314e470bf1e7581d3fbe56ba33df46a7086
Yu-Teng Li <yutengli@berkeley.edu>
2021-09-18 19:54:16 -0700

    Commit front-end before having dinner.

--- webapp/classcaster/src/components/Dashboard.js
+++ webapp/classcaster/src/components/Dashboard.js
@@ -1,13 +1,34 @@
 import React from "react";
-import { withRouter } from "react-router-dom";
+import { Grid, GridItem, Box } from "@chakra-ui/react";
 
 const Dashboard = (props) => {
 
     return (
-        <div>
-            <h1> This is dashboard page. </h1>
-        </div>
+        <Grid
+            h="100vh"
+            templateRows="repeat(7, 1fr)"
+            templateColumns="repeat(8, 1fr)"
+            gap={4}
+            p = {3}
+            >
+            <GridItem rowSpan={7} colSpan={2} bg="tomato" />
+            <GridItem rowSpan={2} colSpan={3} bg="papayawhip" />
+            <GridItem rowSpan={2} colSpan={3} bg="papayawhip" />
+            <GridItem rowSpan={5} colSpan={6} bg="blue.400">
+                <Grid 
+                    templateRows="repeat(4, 1fr)" 
+                    templateColumns="repeat(2, 1fr)" 
+                    p = {3} 
+                    gap = {4} 
+                    height = {"100%"}
+                >
+                    <GridItem rowSpan={1} colSpan={1} bg="tomato"> fdsfd </GridItem>
+                    <GridItem colSpan={1} bg="tomato"> fdsfd </GridItem>
+                    <GridItem colSpan={1} bg="tomato"> fdsfd </GridItem>
+                </Grid>
+            </GridItem>
+        </Grid>
     );
 }
 
-export default withRouter(Dashboard);
+export default Dashboard;
--- webapp/classcaster/src/routers/AppRouter.js
+++ webapp/classcaster/src/routers/AppRouter.js
@@ -5,7 +5,6 @@ import Dashboard from "../components/Dashboard";
 import SignUpPage from "../components/SignUpPage";
 import LogInPage from "../components/LogInPage";
 import NotFound from "../components/NotFound";
-import TestFile from "../components/TestFile";
 
 // export const history = createBrowserHistory();
 
@@ -14,14 +13,13 @@ const AppRouter = () => {
     <Router>
       <div>
         <Switch>
-          <Route path="/" exact component={TestFile} />
+          <Route path="/" exact component={LogInPage} />
           <Route path="/login" exact component={LogInPage} />
           <Route path="/dashboard" exact component={Dashboard} />
-          <Route path="/signup" exact component={SignUpPage} />
           <Route path="/courses" exact component={Courses} />
           {/**<PrivateRoute path = "/courses/:id" exact component = {CoursePage}/>
-                    <PrivateRoute path = "/students/edit/:id" component={EditStudentContact} />
-                    <PrivateRoute path = "/students/content/:id" component = {StudentPersonalPage}/> */}
+            <PrivateRoute path = "/students/edit/:id" component={EditStudentContact} />
+            <PrivateRoute path = "/students/content/:id" component = {StudentPersonalPage}/> */}
           <Route component={NotFound} />
         </Switch>
       </div>
