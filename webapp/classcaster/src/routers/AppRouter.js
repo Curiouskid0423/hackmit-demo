3b75857af0c5890f9f118077de03ffb96ab68f23
Sumer Kohli <sumer.kohli@berkeley.edu>
2021-09-18 17:59:12 -0700

    Fix router bug

--- webapp/classcaster/src/routers/AppRouter.js
+++ webapp/classcaster/src/routers/AppRouter.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Router, Route, Switch } from "react-router-dom";
-// import { createBrowserHistory } from "history";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Courses from "../components/Courses";
 import Dashboard from "../components/Dashboard";
 import SignUpPage from "../components/SignUpPage";
@@ -10,23 +9,23 @@ import NotFound from "../components/NotFound";
 // export const history = createBrowserHistory();
 
 const AppRouter = () => {
-    return (
-        <Router>
-            <div>
-                <Switch>
-                    <Route path = "/" exact component = {LogInPage}/>
-                    <Route path = "/login" exact component = {LogInPage}/>
-                    <Route path = "/dashboard" exact component = {Dashboard}/>
-                    <Route path = "/signup" exact component = {SignUpPage}/>
-                    <Route path = "/courses" exact component = {Courses}/>
-                    {/**<PrivateRoute path = "/courses/:id" exact component = {CoursePage}/>
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" exact component={LogInPage} />
+          <Route path="/login" exact component={LogInPage} />
+          <Route path="/dashboard" exact component={Dashboard} />
+          <Route path="/signup" exact component={SignUpPage} />
+          <Route path="/courses" exact component={Courses} />
+          {/**<PrivateRoute path = "/courses/:id" exact component = {CoursePage}/>
                     <PrivateRoute path = "/students/edit/:id" component={EditStudentContact} />
                     <PrivateRoute path = "/students/content/:id" component = {StudentPersonalPage}/> */}
-                    <Route component = {NotFound} />
-                </Switch>
-            </div>
-        </Router>
-    )
-}
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default AppRouter;
