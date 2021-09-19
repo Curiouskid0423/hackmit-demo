04f44ca71c7daa007277f0370d9d2b81501bb2ee
Sumer Kohli <sumer.kohli@berkeley.edu>
2021-09-19 01:02:59 -0700

    Finish most of backend getCourseAssignments

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,17 +1,31 @@
 var express = require("express");
+const db = require("../db");
 var azure = require("../ml/azure");
 var tangram = require("../ml/tangram");
 var router = express.Router();
 
 router.post("/api", async (req, res, next) => {
-    const {type} = req.body;
-    if (type === "getCourseAssignments") {
-        return res.json([]);
-    } else if (type === "runAzurePredictions") {
-        const {user, course} = req.body;
-        const output = await azure.getRecommendedGroup(user, course);
-        return res.json(output);
-    }
+  const { type } = req.body;
+  if (type === "getCourseAssignments") {
+    // name, numCompleted, numStudents, forecastTime
+    const { course } = req.body;
+    console.log(req.user);
+    const { id } = req.user;
+    const rows = db.getRows(
+      `SELECT assignment_id, count(*) AS numStudents, count(*) filter (where completed) AS numCompleted FROM
+       (
+           SELECT assignment_id, user_id, MAX(completed) AS completed FROM classcaster_schema.times
+           GROUP BY (assignment_id, user_id)
+       )
+       GROUP BY (assignment_id)`,
+      []
+    );
+    return res.json(rows);
+  } else if (type === "runAzurePredictions") {
+    const { user, course } = req.body;
+    const output = await azure.getRecommendedGroup(user, course);
+    return res.json(output);
+  }
 });
 
 module.exports = router;
