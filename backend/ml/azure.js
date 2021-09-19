6d3b785e0ea0e35ad02bea81abfbf6d3c4c728b3
Sumer Kohli <sumer.kohli@berkeley.edu>
2021-09-18 22:07:09 -0700

    Add ML stubs

---
+++ backend/ml/azure.js
@@ -0,0 +1,5 @@
+const db = require("../db/index");
+
+export function getRecommendedGroup(user_id, course_id) {
+  // TODO: Return recommended group for user with id = user_id in course with id = course_id
+}
---
+++ backend/ml/tangram.js
@@ -0,0 +1,9 @@
+const db = require("../db/index");
+
+export function updatePredictedScores() {
+  // TODO: Retrain model
+}
+
+export function getPredictedScore(user_id, assignment_id) {
+  // TODO: Return predicted score for user with id = user_id and assignment with id = assigment_id
+}
