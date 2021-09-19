f41d7581623eccb0a0dbdd31656fd37693240a4a
Sumer Kohli <sumer.kohli@berkeley.edu>
2021-09-19 01:24:46 -0700

    Improve assignments query

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -11,14 +11,20 @@ router.post("/api", async (req, res, next) => {
     const { course } = req.body;
     console.log(req.user);
     const { id } = req.user;
-    const rows = db.getRows(
-      `SELECT assignment_id, count(*) AS numStudents, count(*) filter (where completed) AS numCompleted FROM
+    const assignments = await db.getRows(
+      "SELECT id FROM classcaster_schema.assignments WHERE course_id = $1",
+      [course]
+    );
+    // const assignments_str = ", ".join(assignments.map((x) => `'${x}'`));
+    const rows = await db.getRows(
+      `SELECT assignment_id, count(*) AS numStudents, count(*) filter (where completed) AS numCompleted, completed filter (where completed AND user_id = $1) FROM
        (
            SELECT assignment_id, user_id, MAX(completed) AS completed FROM classcaster_schema.times
+           WHERE assignment_id IN $2
            GROUP BY (assignment_id, user_id)
        )
        GROUP BY (assignment_id)`,
-      []
+      [id, assignments]
     );
     return res.json(rows);
   } else if (type === "runAzurePredictions") {
